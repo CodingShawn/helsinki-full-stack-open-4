99d37334f11d8f585226433907754ff6b6489186
Shawn <codingshawn@gmail.com>
2021-02-22 22:22:53 +0800

    Added test to check if number of blog posts returned in get '/api/blogs' is correct

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -1,14 +1,29 @@
 const mongoose = require('mongoose');
 const supertest = require('supertest');
 const app = require('../app');
+const Blog = require('../models/blog');
+const initialBlogs = require('./test_helper');
 
 const api = supertest(app);
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  let blogObject = new Blog(initialBlogs[0]);
+  await blogObject.save();
+  blogObject = new Blog(initialBlogs[1]);
+  await blogObject.save(0);
+})
 
 test('Blogs are returned as JSON', async() => {
   await api
     .get('/api/blogs')
     .expect(200)
     .expect('Content-Type', /application\/json/)
+})
+
+test('All blogs are returned', async () => {
+  const response = await api.get('/api/blogs');
+  expect(response.body).toHaveLength(2);
 })
 
 afterAll(() => {
