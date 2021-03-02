c90cd3b217e53c7a692dffeaafb4f253000c2bbe
Shawn <codingshawn@gmail.com>
2021-03-02 21:37:12 +0800

    Modified app.js to use usersRouter

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
-const blogRouter = require('./controllers/blogs');
+const blogRouter = require("./controllers/blogs");
 const cors = require("cors");
 const mongoose = require("mongoose");
-const config = require('./utils/confiq')
-const errorHandler = require('./utils/middleware')
+const config = require("./utils/confiq");
+const errorHandler = require("./utils/middleware");
+const usersRouter = require("./controllers/users");
 
 const mongoUrl = config.MONGODB_URI;
 mongoose.connect(mongoUrl, {
@@ -16,7 +17,8 @@ mongoose.connect(mongoUrl, {
 
 app.use(cors());
 app.use(express.json());
-app.use('/api/blogs', blogRouter);
+app.use("/api/blogs", blogRouter);
+app.use("/api/users", usersRouter);
 app.use(errorHandler);
 
 module.exports = app;
