c0e17aa6d9b344ac15b8db0b4bcdaf98db7aaad3
Shawn <codingshawn@gmail.com>
2021-03-08 22:23:13 +0800

    Edited user GET route to return blogs info

--- controllers/users.js
+++ controllers/users.js
@@ -6,7 +6,9 @@ usersRouter.post("/", async (request, response, next) => {
   let body = await request.body;
 
   if (body.password.length < 3) {
-    return response.status(401).json({error: 'Password must be at least 3 characters long'})
+    return response
+      .status(401)
+      .json({ error: "Password must be at least 3 characters long" });
   }
 
   const saltRounds = 10;
@@ -20,14 +22,18 @@ usersRouter.post("/", async (request, response, next) => {
   try {
     const savedUser = await user.save();
     response.json(savedUser);
-  }
-  catch(exception) {
-    next(exception)
+  } catch (exception) {
+    next(exception);
   }
 });
 
 usersRouter.get("/", async (request, response) => {
-  let users = await User.find({});
+  let users = await User.find({}).populate("blogs", {
+    url: 1,
+    title: 1,
+    author: 1,
+    id: 1,
+  });
   response.status(200).json(users);
 });
 
