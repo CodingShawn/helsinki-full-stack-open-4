19d3600ed751fd6b65ea268d096c9d9d2a826a08
Shawn <codingshawn@gmail.com>
2021-03-10 08:09:54 +0800

    Added login controller using tokens

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const mongoose = require("mongoose");
 const config = require("./utils/confiq");
 const errorHandler = require("./utils/middleware");
 const usersRouter = require("./controllers/users");
+const loginRouter = require("./controllers/login");
 
 const mongoUrl = config.MONGODB_URI;
 mongoose.connect(mongoUrl, {
@@ -19,6 +20,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/blogs", blogRouter);
 app.use("/api/users", usersRouter);
+app.use("/api/login", loginRouter);
 app.use(errorHandler);
 
 module.exports = app;
